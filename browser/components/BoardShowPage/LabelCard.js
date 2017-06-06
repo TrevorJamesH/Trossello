b0e9930d09a65643a986d60785fa8ea2ad2ca330
TrevorJamesH <jetsamrover@gmail.com>
2017-06-06 16:34:23 -0700

    wip

---
+++ browser/components/BoardShowPage/LabelCard.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react'
+import $ from 'jquery'
+import Form from '../Form'
+import Button from '../Button'
+import DialogBox from '../DialogBox'
+import Label from './Label'
+import LabelCreate from './LabelCreate'
+import PopoverMenuButton from '../PopoverMenuButton'
+import Icon from '../Icon'
+import './CardModal.sass'
+
+export default class LabelCard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      boardId: this.props.board.id,
+      title: this.props.card.content,
+      labels: this.props.board.labels
+    }
+    this.changeLabelHandler = this.changeLabelHandler.bind(this)
+  }
+
+  changeLabelHandler(event){
+    event.preventDefault()
+    if (this.state.title.replace(/\s+/g,'') === '') { return }
+    $.ajax({
+      method: 'post',
+      url: `/api/boards/${this.state.boardId}/lists/${this.state.listId}/cards`,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify({content: this.state.title}),
+    }).then( card => {
+      $.ajax({
+        method: 'post',
+        url: `/api/cards/${card.id}/move`,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify({boardId: card.board_id, listId: card.list_id, order: this.state.order}),
+      }).then( () => {
+        boardStore.reload()
+        this.props.onClose()
+      })
+    })
+  }
+
+  labelClickHandler(){
+
+  }
+
+  buildLabelList(){
+    return this.state.labels.map( (label, index) => {
+      return <Label label={label} key={index}/>
+    })
+  }
+
+
+  render(){
+    const labelCreate = <LabelCreate board={this.props.board}/>
+    if (this.state.boards === null){
+      return <DialogBox className="CardModal-CopyCardDialog" heading='Labels' onClose={this.props.onClose}>
+        Loading…
+      </DialogBox>
+    }
+
+    const labelList = this.buildLabelList()
+
+    const labelSelector =
+      <div className='CardModal-CopyCardDialog-SelectContainer'>
+        <label className='CardModal-CopyCardDialog-SelectContainer-Label'> List </label>
+      </div>
+
+    return <DialogBox className="CardModal-CopyCardDialog" heading='Labels' onClose={this.props.onClose}>
+      <div>
+      {labelList}
+
+
+      <PopoverMenuButton className="CardModal-controls-copy" type="default" popover={labelCreate}>
+        <Icon type="plus" /> New Label
+      </PopoverMenuButton>
+      </div>
+    </DialogBox>
+  }
+}
---
+++ browser/components/BoardShowPage/LabelCreate.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react'
+import $ from 'jquery'
+import '../CreateBoardPopover.sass'
+import Link from '../Link'
+import Icon from '../Icon'
+import Form from '../Form'
+import Button from '../Button'
+import DialogBox from '../DialogBox'
+
+
+export default class LabelCreate extends Component {
+
+  static contextTypes = {
+    redirectTo: React.PropTypes.func,
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      color: '',
+      name: ''
+    }
+    this.updateColor = this.updateColor.bind(this)
+    this.onClick = this.onClick.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentDidMount(){
+    this.refs.name.focus()
+  }
+
+  updateColor(event, color){
+    if (event.target === this.refs.color)
+      color = event.target.value
+    this.setState({color})
+  }
+
+  onClick(event){
+    event.preventDefault()
+    // this.props.newLabel(event)
+  }
+
+  onSubmit(event){
+    event.preventDefault()
+
+    const label = {
+      name: this.refs.name.value,
+      color: this.refs.color.value,
+    }
+    if (label.name.replace(/\s+/g,'') === '') return
+    $.ajax({
+      method: "POST",
+      url: '/api/labels',
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify(label),
+    })
+  }
+
+  reset(){
+    if (this.refs.name) this.refs.name.value = ''
+    if (this.refs.color) this.refs.color.value = ''
+  }
+
+  render(props){
+    const colorBoxes = colors.map(color =>
+      <ColorBox key={color} color={color} onClick={this.updateColor} />
+    )
+
+    return <DialogBox
+      heading="New Label"
+      onClose={this.props.onClose}
+      className="CreateBoardPopover"
+    >
+      <Form onSubmit={this.onSubmit}>
+        <label>
+          <div>Name</div>
+          <input type="text" ref="name"/>
+        </label>
+        <div className="CreateBoardPopover-createBackgroundColor">
+          {colorBoxes}
+        </div>
+        <label>
+          <input
+            type="text"
+            ref="color"
+            placeholder="#2E86AB"
+            value={this.state.color || ''}
+            onChange={this.updateColor}
+          />
+        </label>
+        <Button type="primary" action="submit">Create</Button>
+      </Form>
+    </DialogBox>
+  }
+}
+
+const ColorBox = (props) => {
+  const {onClick, color} = props
+  return <div
+    onClick={(event)=>{ onClick(event, color) }}
+    style={{backgroundColor: color}}
+    className="CreateBoardPopover-createBackgroundColor-box"
+  />
+}
+
+const colors = [
+  "#0079bf",
+  "#61bd4f",
+  "#f2d600",
+  "#ffab4a",
+  "#eb5a46",
+  "#c377e0",
+  "#ff80ce",
+  "#00c2e0"
+]
